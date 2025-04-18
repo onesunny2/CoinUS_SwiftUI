b9008247a4aa20de55166fb27701451c11f11ff2
onesunny2 <ippne996@gmail.com>
2025-04-18 16:35:14 +0900

    first commit: add gitignore, set basic

---
+++ CoinUS_SwiftUI/CoinUS_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinUS_SwiftUIApp.swift
+//  CoinUS_SwiftUI
+//
+//  Created by Lee Wonsun on 4/18/25.
+//
+
+import SwiftUI
+
+@main
+struct CoinUS_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoinUS_SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CoinUS_SwiftUI
+//
+//  Created by Lee Wonsun on 4/18/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
