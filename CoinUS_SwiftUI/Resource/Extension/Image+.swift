4ea9b8ec70ed47a49ef3d10d406909e031575baa
onesunny2 <ippne996@gmail.com>
2025-04-19 23:56:12 +0900

    Image와 Text 타입의 기본 세팅 extension 구현

--- CoinUS_SwiftUI/Resource/Extension/Image+.swift
+++ CoinUS_SwiftUI/Resource/Extension/Image+.swift
@@ -5,4 +5,13 @@
 //  Created by Lee Wonsun on 4/19/25.
 //
 
-import Foundation
+import SwiftUI
+
+extension Image {
+    
+    func setBasic(ratio: CGFloat = 1.0, content mode: ContentMode = .fill) -> some View {
+        self
+            .resizable()
+            .aspectRatio(ratio, contentMode: mode)
+    }
+}
--- CoinUS_SwiftUI/Resource/Extension/Text+.swift
+++ CoinUS_SwiftUI/Resource/Extension/Text+.swift
@@ -5,4 +5,13 @@
 //  Created by Lee Wonsun on 4/19/25.
 //
 
-import Foundation
+import SwiftUI
+
+extension Text {
+    
+    func setBasic<S: ShapeStyle>(size: CGFloat, weight: Font.Weight, color: S = .foreground) -> some View {
+        self
+            .font(.system(size: size, weight: weight))
+            .foregroundStyle(color)
+    }
+}
