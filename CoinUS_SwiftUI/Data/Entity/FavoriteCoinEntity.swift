c44e64b0708c156f0ac90a73858532142c6d804f
onesunny2 <ippne996@gmail.com>
2025-04-19 23:57:03 +0900

    즐겨찾기 셀에 사용할 entity, repository 생성 및 더미데이터 레포지토리 추가

--- CoinUS_SwiftUI/Data/Entity/FavoriteCoinEntity.swift
+++ CoinUS_SwiftUI/Data/Entity/FavoriteCoinEntity.swift
@@ -5,4 +5,40 @@
 //  Created by Lee Wonsun on 4/19/25.
 //
 
-import Foundation
+import SwiftUI
+
+struct FavoriteCoinEntity: Identifiable, Hashable {
+    let id: String
+    let name: String
+    let symbol: String
+    let image: String
+    let currentPrice: String
+    let changePercentage: String // 24시간 변동폭
+    
+    var percentageStatus: PercentageStatus {
+        if changePercentage.contains("-") {
+            return .음수
+        } else if changePercentage.contains("+") {
+            return .양수
+        }
+        else {
+            return .제로
+        }
+    }
+    
+    var statusColor: Color {
+        switch percentageStatus {
+        case .음수: return .blue
+        case .제로: return .primary
+        case .양수: return .red
+        }
+    }
+    
+    var backgroundColor: Color {
+        switch percentageStatus {
+        case .음수: return .blue.opacity(0.1)
+        case .제로: return .primary.opacity(0.1)
+        case .양수: return .red.opacity(0.1)
+        }
+    }
+}
--- CoinUS_SwiftUI/Data/Repository/FavoriteRepository.swift
+++ CoinUS_SwiftUI/Data/Repository/FavoriteRepository.swift
@@ -6,3 +6,57 @@
 //
 
 import Foundation
+
+protocol FavoriteRepository: AnyObject {
+    func getFavoriteInfo(type: FavoriteType) -> [FavoriteCoinEntity]
+}
+
+final class DummyFavoriteRepository: FavoriteRepository {
+    func getFavoriteInfo(type: FavoriteType) -> [FavoriteCoinEntity] {
+        switch type {
+        default:
+            return [
+                FavoriteCoinEntity(
+                    id: "bitcoin",
+                    name: "Bitcoin",
+                    symbol: "BTC",
+                    image: "https://coin-images.coingecko.com/coins/images/1/large/bitcoin.png?1696501400",
+                    currentPrice: "120,970,064",
+                    changePercentage: "+0.71%"
+                ),
+                FavoriteCoinEntity(
+                    id: "ethereum",
+                    name: "Ethereum",
+                    symbol: "ETH",
+                    image: "https://coin-images.coingecko.com/coins/images/279/large/ethereum.png?1696501628",
+                    currentPrice: "4,567,890",
+                    changePercentage: "-1.23%"
+                ),
+                FavoriteCoinEntity(
+                    id: "binancecoin",
+                    name: "Binance Coin",
+                    symbol: "BNB",
+                    image: "https://coin-images.coingecko.com/coins/images/825/large/binance-coin.png?1696501843",
+                    currentPrice: "789,123",
+                    changePercentage: "+2.45%"
+                ),
+                FavoriteCoinEntity(
+                    id: "solana",
+                    name: "Solana",
+                    symbol: "SOL",
+                    image: "https://coin-images.coingecko.com/coins/images/4128/large/solana.png?1696504756",
+                    currentPrice: "234,567",
+                    changePercentage: "-0.89%"
+                ),
+                FavoriteCoinEntity(
+                    id: "cardano",
+                    name: "Cardano",
+                    symbol: "ADA",
+                    image: "https://coin-images.coingecko.com/coins/images/975/large/cardano.png?1696502941",
+                    currentPrice: "1,234",
+                    changePercentage: "+1.67%"
+                )
+            ]
+        }
+    }
+}
