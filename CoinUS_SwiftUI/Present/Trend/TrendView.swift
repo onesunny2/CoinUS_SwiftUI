efa2b206d8ef61c7d5f4a7ff4f623500cfbfe22f
onesunny2 <ippne996@gmail.com>
2025-04-19 23:58:51 +0900

    지금까지 구성한 요소들로 TrendView 구현(좋아요 미포함)

--- CoinUS_SwiftUI/Present/Trend/TrendView.swift
+++ CoinUS_SwiftUI/Present/Trend/TrendView.swift
@@ -8,11 +8,38 @@
 import SwiftUI
 
 struct TrendView: View {
+    
+    let dummyData: TrendRepository
+
     var body: some View {
-        Text("Trend View")
+        ScrollView {
+            VStack(alignment: .leading, spacing: 40) {
+                favoriteTitle
+                TrendTopGridCell(title: StringLiteral.topCoin.text, trendData: dummyData, trendType: .coin)
+                TrendTopGridCell(title: StringLiteral.topNft.text, trendData: dummyData, trendType: .nft)
+            }
+        }
+    }
+    
+    private var favoriteTitle: some View {
+        Text(StringLiteral.favorite.text)
+            .setBasic(size: 20, weight: .bold)
+            .padding(.leading, 20)
+    }
+}
+
+extension TrendView {
+    enum StringLiteral: String {
+        case favorite = "My Favorite"
+        case topCoin = "Top15 Coin"
+        case topNft = "Top7 NFT"
+        
+        var text: String {
+            return self.rawValue
+        }
     }
 }
 
 #Preview {
-    TrendView()
+    TrendView(dummyData: DummyTrendRepository())
 }
