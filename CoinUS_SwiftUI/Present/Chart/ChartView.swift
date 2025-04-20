ff9675bc32ab0d920b9ebba91a7e55d23004ffab
onesunny2 <ippne996@gmail.com>
2025-04-21 04:00:24 +0900

    chartView navigation ToobarITem 구현

--- CoinUS_SwiftUI/Present/Chart/ChartView.swift
+++ CoinUS_SwiftUI/Present/Chart/ChartView.swift
@@ -8,8 +8,38 @@
 import SwiftUI
 
 struct ChartView: View {
+    
+    @Environment(\.dismiss) private var dismiss
+    
     var body: some View {
         Text("ChartView")
+            .navigationBarBackButtonHidden(true)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    toolBarLeft
+                }
+                ToolbarItem(placement: .topBarTrailing) {
+                    tooBarRight
+                }
+            }
+    }
+    
+    private var toolBarLeft: some View {
+        Button {
+            dismiss()
+        } label: {
+            Image(systemName: ImageLiterals.chevronLeft)
+                .setSystemImage(size: 18, weight: .regular)
+        }
+    }
+    
+    private var tooBarRight: some View {
+        Button {
+            print("즐겨찾기 버튼")
+        } label: {
+            Image(systemName: ImageLiterals.star)
+                .setSystemImage(size: 15, weight: .regular)
+        }
     }
 }
 
