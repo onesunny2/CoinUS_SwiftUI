dde4ba712d1f0b45c0d2fcfd3254e787aecece45
onesunny2 <ippne996@gmail.com>
2025-04-20 19:23:42 +0900

    ViewModelType 프로토콜 생성 후 이를 적용한 ViewModel 구현

--- CoinUS_SwiftUI/Present/Trend/ViewModel/TrendViewModel.swift
+++ CoinUS_SwiftUI/Present/Trend/ViewModel/TrendViewModel.swift
@@ -6,3 +6,70 @@
 //
 
 import Foundation
+import Combine
+
+final class TrendViewModel: ViewModelType {
+    
+    var input = Input()
+    @Published var output = Output()
+    
+    var cancellables = Set<AnyCancellable>()
+    
+    private let trendRepository: TrendRepository
+    private let favoriteRepository: FavoriteRepository
+    
+    init(trendRepository: TrendRepository, favoriteRepository: FavoriteRepository) {
+        self.trendRepository = trendRepository
+        self.favoriteRepository = favoriteRepository
+        
+        transform()
+    }
+}
+
+extension TrendViewModel {
+    
+    struct Input {
+        let onAppear = PassthroughSubject<Void, Never>()
+    }
+    
+    struct Output {
+        var favoriteItems: [FavoriteCoinEntity] = []
+        var trendCoinItems: [TrendTOPEntity] = []
+        var trendNftItems: [TrendTOPEntity] = []
+    }
+    
+    func transform() {
+        input.onAppear
+            .sink { [weak self] _ in
+                guard let self else { return }
+                self.getTrendCoinInfo()
+                self.getTrendNftInfo()
+                self.getFavoriteCoinInfo()
+            }
+            .store(in: &cancellables)
+    }
+    
+    private func getTrendCoinInfo() {
+        output.trendCoinItems = trendRepository.getTrendInfo(type: .coin)
+    }
+    
+    private func getTrendNftInfo() {
+        output.trendNftItems = trendRepository.getTrendInfo(type: .nft)
+    }
+    
+    private func getFavoriteCoinInfo() {
+        output.favoriteItems = favoriteRepository.getFavoriteInfo(type: .trend)
+    }
+}
+
+extension TrendViewModel {
+    enum Action {
+        case onAppear
+    }
+    
+    func action(_ action: Action) {
+        switch action {
+        case .onAppear: input.onAppear.send(())
+        }
+    }
+}
--- CoinUS_SwiftUI/Source/Protocol/ViewModelType.swift
+++ CoinUS_SwiftUI/Source/Protocol/ViewModelType.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+import Combine
+
+protocol ViewModelType: AnyObject, ObservableObject {
+    associatedtype Input
+    associatedtype Output
+    
+    var cancellables: Set<AnyCancellable> { get set }
+    
+    var input: Input { get set }
+    var output: Output { get set }
+    
+    func transform()
+}
