a528a6f47924652c8146ec8280278fcd547deb13
onesunny2 <ippne996@gmail.com>
2025-04-21 00:44:49 +0900

    기본적인 SearchViewModel 구현 및 검색어 input 시 repository에서 데이터 받아오는 메서드 구현

--- CoinUS_SwiftUI/Present/Search/ViewModel/SearchViewModel.swift
+++ CoinUS_SwiftUI/Present/Search/ViewModel/SearchViewModel.swift
@@ -6,3 +6,60 @@
 //
 
 import Foundation
+import Combine
+
+final class SearchViewModel: ViewModelType {
+    
+    var input = Input()
+    @Published var output = Output()
+    
+    var cancellables = Set<AnyCancellable>()
+    
+    private let searchRepository: SearchRepository
+    
+    init(searchRepository: SearchRepository) {
+        self.searchRepository = searchRepository
+        
+        transform()
+    }
+}
+
+extension SearchViewModel {
+    
+    struct Input {
+        var tappedSearchButton = PassthroughSubject<String, Never>()
+        var tappedFavoriteButton = PassthroughSubject<String, Never>()
+    }
+    
+    struct Output {
+        var searchResults: [SearchCoinEntity] = []
+    }
+    
+    func transform() {
+        input.tappedSearchButton
+            .sink { [weak self] keyword in
+                guard let self else { return }
+                // TODO: 실제 네트워크 통신 시에는 키워드에 대한 결과 나오도록 잘 받아야 함
+                self.getSearchResults(keyword)
+            }
+            .store(in: &cancellables)
+    }
+    
+    private func getSearchResults(_ keyword: String) {
+        output.searchResults = searchRepository.getSearchInfo(keyword)
+    }
+}
+
+extension SearchViewModel {
+    enum Action {
+        case search(keyword: String)
+        case favorite(id: String)
+    }
+    
+    func action(_ action: Action) {
+        switch action {
+        case let .search(keyword): input.tappedSearchButton.send(keyword)
+        case let .favorite(id): input.tappedFavoriteButton.send(id)
+        }
+    }
+}
