7744ac938383b1e086eff36e6ba69c32926bb429
onesunny2 <ippne996@gmail.com>
2025-04-21 20:56:24 +0900

    NavigationBarModifier 구현

--- CoinUS_SwiftUI/Present/Component/CustomModifier/NavigationBarModifier.swift
+++ CoinUS_SwiftUI/Present/Component/CustomModifier/NavigationBarModifier.swift
@@ -5,4 +5,39 @@
 //  Created by Lee Wonsun on 4/21/25.
 //
 
-import Foundation
+import SwiftUI
+
+private struct NavigationBarWrapper<Leading: View, Trailing: View>: ViewModifier {
+  private let leading: Leading
+  private let trailing: Trailing
+
+  init(
+    @ViewBuilder leading: () -> Leading,
+    @ViewBuilder trailing: () -> Trailing
+  ) {
+    self.leading = leading()
+    self.trailing = trailing()
+  }
+
+  func body(content: Content) -> some View {
+    if #available(iOS 14, *) {
+      content
+        .toolbar {
+          ToolbarItem(placement: .topBarLeading, content: { leading })
+          ToolbarItem(placement: .topBarTrailing, content: { trailing })
+        }
+    } else {
+      content
+        .navigationBarItems(leading: leading, trailing: trailing)
+    }
+  }
+}
+
+extension View {
+  func navigationBar(
+    @ViewBuilder leading: () -> some View = { EmptyView() },
+    @ViewBuilder trailing: () -> some View = { EmptyView() }
+  ) -> some View {
+    modifier(NavigationBarWrapper(leading: leading, trailing: trailing))
+  }
+}
