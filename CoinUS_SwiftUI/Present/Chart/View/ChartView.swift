73887a315910a2b3e9adf7ae68b179730d458205
onesunny2 <ippne996@gmail.com>
2025-04-21 20:55:32 +0900

    ChartVM에 의존성 주입하여 CharView 구현

--- CoinUS_SwiftUI/Present/Chart/View/ChartView.swift
+++ CoinUS_SwiftUI/Present/Chart/View/ChartView.swift
@@ -6,22 +6,26 @@
 //
 
 import SwiftUI
+import DGCharts
 
 struct ChartView: View {
     
     @Environment(\.dismiss) private var dismiss
+    @StateObject var viewModel: ChartViewModel
     
     var body: some View {
-        Text("ChartView")
-            .navigationBarBackButtonHidden(true)
-            .toolbar {
-                ToolbarItem(placement: .topBarLeading) {
-                    toolBarLeft
-                }
-                ToolbarItem(placement: .topBarTrailing) {
-                    tooBarRight
-                }
-            }
+        chartContent
+        .padding(.horizontal, 20)
+        .navigationBarBackButtonHidden(true)
+        .navigationBar(leading: {
+            toolBarLeft
+        }, trailing: {
+            tooBarRight
+        })
+        .onAppear {
+            viewModel.action(.onAppear)
+        }
+        .redacted(reason: viewModel.output.isLoading ? .placeholder : [])
     }
     
     private var toolBarLeft: some View {
@@ -35,14 +39,119 @@ struct ChartView: View {
     
     private var tooBarRight: some View {
         Button {
-            print("즐겨찾기 버튼")
+            viewModel.action(.favorite)
         } label: {
-            Image(systemName: ImageLiterals.star)
+            Image(systemName: (viewModel.output.chartInfo?.isFavorite ?? false) ? ImageLiterals.starFill : ImageLiterals.star)
                 .setSystemImage(size: 15, weight: .regular)
+        }
+    }
+    
+    @ViewBuilder
+    private var chartContent: some View {
+        VStack(alignment: .leading) {
+            if let coin = viewModel.output.chartInfo {
+                topName(coin)
+                todayPrice(coin)
+                priceInfo(coin)
+                    .padding(.top, 12)
+                chartView
+                updateText(coin)
+            }
+        }
+    }
+    
+    private func topName(_ coin: ChartCoinEntity) -> some View {
+        HStack {
+            IconImageCell(url: coin.image)
+            Text(coin.name)
+                .setBasic(size: 30, weight: .semibold)
+            Spacer()
+        }
+    }
+    
+    private func todayPrice(_ coin: ChartCoinEntity) -> some View {
+        VStack(alignment: .leading, spacing: 4) {
+            Text(coin.currentPrice)
+                .setBasic(size: 30, weight: .semibold)
+            
+            HStack(alignment: .center, spacing: 6) {
+                Text(coin.changePercentage)
+                    .setBasic(size: 16, weight: .medium, color: coin.statusColor)
+                Text(StringLiteral.오늘.text)
+                    .setBasic(size: 16, weight: .medium, color: .gray)
+            }
+        }
+    }
+    
+    private func priceInfo(_ coin: ChartCoinEntity) -> some View {
+        HStack(spacing: 80) {
+            priceHstackCell(
+                StringLiteral.고가.text,
+                coin.high24h,
+                StringLiteral.신고점.text,
+                coin.allTimeHighPrice
+            )
+            
+            priceHstackCell(
+                StringLiteral.저가.text,
+                coin.low24h,
+                StringLiteral.신저점.text,
+                coin.allTimeLowPrice
+            )
+        }
+    }
+    
+    @ViewBuilder
+    private var chartView: some View {
+        if let data = viewModel.output.lineChartData {
+            CoinChartView(chartDataSet: data.0, chartData: data.1)
+                .frame(maxWidth: .infinity)
+        }
+    }
+    
+    private func updateText(_ coin: ChartCoinEntity) -> some View {
+        HStack {
+            Spacer()
+            Text(coin.lastUpdate)
+                .setBasic(size: 12, weight: .semibold, color: .gray)
+        }
+        .padding([.trailing, .bottom], 20)
+    }
+    
+    private func priceHstackCell(
+        _ topTitle: String,
+        _ topPrice: String,
+        _ btmTitle: String,
+        _ btmPrice: String
+    ) -> some View {
+        VStack(alignment: .leading, spacing: 12) {
+            VStack(alignment: .leading, spacing: 6) {
+                Text(topTitle)
+                    .setBasic(size: 18, weight: .semibold, color: (topTitle == StringLiteral.고가.text) ? .red : .blue)
+                Text(topPrice)
+                    .setBasic(size: 18, weight: .regular)
+            }
+            
+            VStack(alignment: .leading, spacing: 6) {
+                Text(btmTitle)
+                    .setBasic(size: 18, weight: .semibold, color: (btmTitle == StringLiteral.신고점.text) ? .red : .blue)
+                Text(btmPrice)
+                    .setBasic(size: 18, weight: .regular)
+            }
         }
     }
 }
 
-#Preview {
-    ChartView()
+extension ChartView {
+    enum StringLiteral: String {
+        case 오늘 = "Today"
+        case 고가
+        case 저가
+        case 신고점
+        case 신저점
+        
+        var text: String {
+            return self.rawValue
+        }
+    }
 }
--- CoinUS_SwiftUI/Present/Chart/ViewModel/ChartViewModel.swift
+++ CoinUS_SwiftUI/Present/Chart/ViewModel/ChartViewModel.swift
@@ -6,3 +6,104 @@
 //
 
 import Foundation
+import Combine
+import DGCharts
+
+final class ChartViewModel: ViewModelType {
+    
+    var input = Input()
+    @Published var output = Output()
+    
+    var cancellables = Set<AnyCancellable>()
+    
+    private let id: String
+    private let chartRepository: ChartRepository
+    private let appStorageManager = AppStorageManager.shared
+    
+    init(
+        id: String,
+        chartRepository: ChartRepository
+    ) {
+        self.id = id
+        self.chartRepository = chartRepository
+        transform()
+    }
+}
+
+extension ChartViewModel {
+    
+    typealias Chart = (LineChartDataSet, LineChartData)
+    
+    struct Input {
+        var onAppear = PassthroughSubject<Void, Never>()
+        var tappedFavoriteButton = PassthroughSubject<Void, Never>()
+    }
+    
+    struct Output {
+        var chartInfo: ChartCoinEntity?
+        var isLoading: Bool = false
+        var lineChartData: Chart?
+    }
+    
+    func transform() {
+        input.onAppear
+            .sink { [weak self] _ in
+                guard let self else { return }
+                Task {
+                    await self.getChartData()
+                }
+            }
+            .store(in: &cancellables)
+        
+        input.tappedFavoriteButton
+            .sink { [weak self] _ in
+                guard let self else { return }
+                self.appStorageManager.AddRemoveFavoriteItem(id)
+                Task {
+                    self.output.isLoading = true
+                    await self.getChartData()
+                    self.output.isLoading = false
+                }
+            }
+            .store(in: &cancellables)
+    }
+    
+    private func getChartData() async {
+        let chartInfo = await chartRepository.getChartCoinInfo(id)
+        output.chartInfo = chartInfo
+        output.lineChartData = getChartData(weekPrice: chartInfo?.sparkLine ?? [])
+    }
+    
+    private func getChartData(weekPrice: [Double]) -> Chart {
+        
+        var chartEntry: [ChartDataEntry] = []
+        
+        let priceDataEntry = weekPrice.map {
+            ((weekPrice.firstIndex(of: $0) ?? 0) + 1, $0)
+        }
+        
+        priceDataEntry.forEach {
+            let chartData = ChartDataEntry(x: Double($0.0), y: $0.1)
+            chartEntry.append(chartData)
+        }
+        
+        let chartDataSet = LineChartDataSet(entries: chartEntry)
+        let chartData = LineChartData(dataSet: chartDataSet)
+        
+        return (chartDataSet, chartData)
+    }
+}
+
+extension ChartViewModel {
+    enum Action {
+        case onAppear
+        case favorite
+    }
+    
+    func action(_ action: Action) {
+        switch action {
+        case .onAppear: input.onAppear.send(())
+        case .favorite: input.tappedFavoriteButton.send(())
+        }
+    }
+}
