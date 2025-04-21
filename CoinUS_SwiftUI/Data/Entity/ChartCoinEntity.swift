e9a461b8cdbc425df3e2545e99cd56309edbdca9
onesunny2 <ippne996@gmail.com>
2025-04-21 20:54:55 +0900

    ChartCoinEntity로 DefaultChartRepository 구현

--- CoinUS_SwiftUI/Data/Entity/ChartCoinEntity.swift
+++ CoinUS_SwiftUI/Data/Entity/ChartCoinEntity.swift
@@ -5,4 +5,38 @@
 //  Created by Lee Wonsun on 4/21/25.
 //
 
-import Foundation
+import SwiftUI
+
+struct ChartCoinEntity {
+    let id: String
+    let name: String
+    let image: String
+    let currentPrice: String
+    let changePercentage: String
+    let high24h: String
+    let low24h: String
+    let allTimeHighPrice: String
+    let allTimeLowPrice: String
+    let sparkLine: [Double]
+    let lastUpdate: String
+    let isFavorite: Bool
+    
+    var percentageStatus: PercentageStatus {
+        if changePercentage.contains("-") {
+            return .음수
+        } else if changePercentage.contains("+") {
+            return .양수
+        }
+        else {
+            return .제로
+        }
+    }
+    
+    var statusColor: Color {
+        switch percentageStatus {
+        case .음수: return .blue
+        case .제로: return .primary
+        case .양수: return .red
+        }
+    }
+}
--- CoinUS_SwiftUI/Data/Repository/ChartRepository.swift
+++ CoinUS_SwiftUI/Data/Repository/ChartRepository.swift
@@ -6,3 +6,45 @@
 //
 
 import Foundation
+
+protocol ChartRepository: AnyObject {
+    func getChartCoinInfo(_ id: String) async -> ChartCoinEntity?
+}
+
+final class DefaultChartRepository: ChartRepository {
+    func getChartCoinInfo(_ id: String) async -> ChartCoinEntity? {
+        
+        let network = NetworkManager.shared
+        let appStorage = AppStorageManager.shared
+        let savedList = appStorage.loadFavoriteItem()
+        
+        do {
+            let api = NetworkURL.market(ids: [id])
+            let markets: [GeckoMarket] = try await network.request(api: api)
+
+            guard let market = markets.first else { return nil }
+            
+            let coinEntity: ChartCoinEntity = ChartCoinEntity(
+                id: market.id,
+                name: market.name,
+                image: market.image,
+                currentPrice: market.currentPrice.translate₩Formatted(),
+                changePercentage: market.pricePercentage.translateChangeRate(),
+                high24h: market.highPriceAllDay.translate₩Formatted(),
+                low24h: market.lowPriceAllDay.translate₩Formatted(),
+                allTimeHighPrice: market.allTimeHighPrice.translate₩Formatted(),
+                allTimeLowPrice: market.allTimeLowPrice.translate₩Formatted(),
+                sparkLine: market.weeklyPrice.price,
+                lastUpdate: market.updateDate.toFormattedDate(),
+                isFavorite: savedList.contains(market.id)
+            )
+            
+            
+            return coinEntity
+        } catch {
+            print(error)
+            
+            return nil
+        }
+    }
+}
